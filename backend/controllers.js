cb75ec2142b480ec9c0b8638430a95a63fbd6f6c
Vrinda <mac@Macs-MacBook-Pro.local>
2021-10-23 16:46:48 +0530

    basic app

--- backend/controllers.js
+++ backend/controllers.js
@@ -7,17 +7,18 @@ exports.login = async (req,res)=>{
         const { name, password } = req.body
         if(name && password)
         {
-            const { username, passwordHash, userId, privateKey } = process.env
+            const { username, passwordHash, privateKey } = process.env
             let match  = await bcrypt.compare(password,passwordHash)
             if(name===username && match)
             {
-                console.log('now we sign')
                 const token = await jwt.sign(
-                    { userId:userId }, 
+                    { username:username }, 
                     privateKey,
                     { expiresIn: '1h'}
                 )
-                if(token) return res.json(token)
+                if(token) {
+                    return res.json({token:token, username:username})
+                }
             }
         }
         return res.sendStatus(401)        
@@ -28,45 +29,52 @@ exports.login = async (req,res)=>{
 }
 
 exports.verifyToken = (req,res, next)=>{
-    try{
-        req.user = {userId:null, verified:false}
-        const { privateKey } = process.env
-        const bearerHeader = req.headers['authorization']
-        if(typeof bearerHeader!=='undefined')
-        {
-            const bearerToken = bearerHeader.split(' ')[1]
-            jwt.verify(bearerToken, privateKey, function (err,data){
-                if(! (err && typeof data=== 'undefined'))
-                    req.user = {userId:data.userId, verified:true}
-            })
-        }
-        next()
-    }catch(err) {
-        console.log(err)
-        res.sendStatus(500)
+    req.user = {username:null, verified:false}
+    const { privateKey } = process.env
+    const bearerHeader = req.headers['authorization']
+    if(typeof bearerHeader!=='undefined')
+    {
+        const bearerToken = bearerHeader.split(' ')[1]
+        jwt.verify(bearerToken, privateKey, function (err,data){
+            if(! (err && typeof data=== 'undefined'))
+                {
+                    req.user = {username:data.username, verified:true}
+                    next()
+                }
+            else {
+                console.log(err.name)
+                return res.sendStatus(403)
+            }
+        })
     }
 }
 
 exports.updatePassword = async(req,res)=> {
    try{
-       if(req.user.verified){
-            const { oldPassword, newPassword } = req.body
-            const { passwordHash } = process.env
+        const { oldPassword, newPassword } = req.body
+        const { passwordHash } = process.env
 
-            if(oldPassword && newPassword)
+        if(oldPassword && newPassword)
+        {
+            let match = await bcrypt.compare(oldPassword, passwordHash)
+            if(match) 
             {
-                let match = await bcrypt.compare(oldPassword, passwordHash)
-                if(match) 
-                {
-                    let hash = await bcrypt.hash(newPassword, saltRounds)
-                    if (hash) return res.json(hash)
-                }
+                let hash = await bcrypt.hash(newPassword, saltRounds)
+                if (hash) return res.json(hash)
             }
         }
         return res.sendStatus(401)
-
     } catch(err){
         console.log(err)
         return res.sendStatus(500)
     }
+}
+
+exports.logout = (req,res) => {
+    const bearerHeader = req.headers['authorization']
+    if(typeof bearerHeader!=='undefined'){
+        const bearerToken = bearerHeader.split(' ')[1]
+        //add bearerToken to blacklist
+    }
+    return res.sendStatus(200)
 }
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { login, verifyToken, updatePassword } = require('./controllers')
+const { login, verifyToken, updatePassword, logout } = require('./controllers')
 
 const PORT = process.env.PORT || 5000
 const app =express()
@@ -11,7 +11,7 @@ app.use(express.json()) //accept json data with post requests
 //routes
 app.post('/login',login)
 app.post('/update', verifyToken, updatePassword)
-
+app.get('/logout', logout)
 
 //start server
 app.listen(PORT, ()=>{
